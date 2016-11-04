9787a2de95778d98d261990e6c656f292483e3bb
Daithi McGibbon <daithimcgibbon@gmail.com>
2016-11-04 16:07:58 +0000

    Login updates

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -10,7 +10,16 @@ router.post('/', function(req, res, next) {
     if ((!req.body.username) || (!req.body.password)) {
 
     } else {
-
+        var client = ldapAuth.createClient({
+            url: 'LDAP://gldc-ad-gc-vip.igslb.allstate.com:3268'
+        })
+        client.bind(req.body.username, req.body.password, function(err) {
+            if (err) {
+                console.log('FAILURE: ' + err)
+            } else {
+                console.log('SUCCESS');
+            }
+        })
     }
 
 });
